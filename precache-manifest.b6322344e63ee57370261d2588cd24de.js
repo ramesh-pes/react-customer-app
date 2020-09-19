ce34dc9096c2ab9ef885022604e33a68a65adcc6
ramesh-pes <ramesh.19.dvg@gmail.com>
2020-09-19 17:00:05 +0530

    Updates

---
+++ precache-manifest.b6322344e63ee57370261d2588cd24de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71db67b1bd06adcd2a2a8a718e3c89ad",
+    "url": "/react-customer-app/index.html"
+  },
+  {
+    "revision": "713d87cf1bf8c7d5ea6b",
+    "url": "/react-customer-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1fd0a49a379a37dc14f5",
+    "url": "/react-customer-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "713d87cf1bf8c7d5ea6b",
+    "url": "/react-customer-app/static/js/2.6dfbfc87.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-customer-app/static/js/2.6dfbfc87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd0a49a379a37dc14f5",
+    "url": "/react-customer-app/static/js/main.6d4f027a.chunk.js"
+  },
+  {
+    "revision": "e8fee7b6fa4c051b3924",
+    "url": "/react-customer-app/static/js/runtime-main.1d9b0baa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ramesh-pes/react-customer-app/precache-manifest.8de6ae1517b8cfc8907ced6ac38e457d.js"
+  "/react-customer-app/precache-manifest.b6322344e63ee57370261d2588cd24de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ramesh-pes/react-customer-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-customer-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
